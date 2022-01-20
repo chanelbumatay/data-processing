baabcfb0e01ce47ddcce742d5258f8fc744535db
chanel bumatay <91562981+chanelbumatay@users.noreply.github.com>
2022-01-20 11:45:14 -0800

    added widgets and totaks

--- JS/data-processing.js
+++ JS/data-processing.js
@@ -1,8 +1,10 @@
 //use location object to access querystring (address bar)
 const queryString = window.location.search;
 
-if(queryString !== ""){//process data
-      let myData = ''; 
+if(queryString !== ""){ //process data
+      let myData = ''; //with store data for output
+      let myCart =''; //will store cart details
+      let myTotal = 0; //with store total cost
 
 
       //output to console    
@@ -14,14 +16,41 @@ if(queryString !== ""){//process data
 
       urlParams.forEach(function(value, key) {
 
-      //https://stackoverflow.com/questions/542232/in-javascript-how-can-i-perform-a-global-replace-on-string-with-a-variable-insi
-      //will replace underscore with spaces
-      key = key.split("_").join(" ");
+          if(key=="Cart"){//Process cart
+              switch(value){
+                  case "Widget":
+                      myCart += "Widget: $3.99<br />";
+                      myTotal += 3.99;
+                  break;
 
-          myData += `<p>${key}: ${value}</p>`;
-        //console.log(value, key);
-      });
-      myData += '<p><a href="index.html">CLEAR</a></p>'
-      document.getElementById("output").innerHTML = myData;
+                  case "Sprocket" :
+                      myCart += "Sprocket: $5.99<br />";
+                      myTotal += 5.99;
+                  break;
+
+                  case "Thingy" :
+                      myCart += "Thingy: $1.99<br />";
+                      myTotal += 1.99;
+              
+              }
+
+          }else{//build Shipping info
+                //https://stackoverflow.com/questions/542232/in-javascript-how-can-i-perform-a-global-replace-on-string-with-a-variable-insi
+                //will replace underscore with spaces
+            key = key.split("_").join(" ");
+            myData += `<p>${key}: ${value}</p>`;
+           
+          }
+  
+  });
+        myCart = `
+          <p><b>Cart Contents</b></p>
+          <p>${myCart}</p>
+          <p>Total: $${myTotal}</p>
+      `;
+
+        myData = myCart + "<p><b>Shipping Information </b></p>" + myData;
+        myData += '<p><a href="index.html">CLEAR</a></p>';
+        document.getElementById("output").innerHTML = myData;
 }
 
