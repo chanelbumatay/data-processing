8d492a705209286777f6c351d20c303342cfccd0
chanel bumatay <91562981+chanelbumatay@users.noreply.github.com>
2022-01-25 11:42:47 -0800

    updated with case

--- JS/data-processing.js
+++ JS/data-processing.js
@@ -1,4 +1,12 @@
 //use location object to access querystring (address bar)
+function titleCase(str){
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i< str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(' ');
+}
+
 const queryString = window.location.search;
 
 if(queryString !== ""){ //process data
@@ -6,6 +14,7 @@ if(queryString !== ""){ //process data
       let myCart =''; //will store cart details
       let myTotal = 0; //with store total cost
 
+   
 
       //output to console    
       //console.log(queryString);
@@ -16,6 +25,7 @@ if(queryString !== ""){ //process data
 
       urlParams.forEach(function(value, key) {
 
+            
           if(key=="Cart"){//Process cart
               switch(value){
                   case "Widget":
@@ -31,17 +41,27 @@ if(queryString !== ""){ //process data
                   case "Thingy" :
                       myCart += "Thingy: $1.99<br />";
                       myTotal += 1.99;
-              
+                  break;
               }
-
+              
           }else{//build Shipping info
                 //https://stackoverflow.com/questions/542232/in-javascript-how-can-i-perform-a-global-replace-on-string-with-a-variable-insi
                 //will replace underscore with spaces
+            switch(key){
+                case "First_Name":
+                case "Last_Name":
+                case "Address":
+                case "City":
+                    value = titleCase(value);
+                break;
+            }
+                key = key.split("_").join(" ");
+                
+            }
+            
             key = key.split("_").join(" ");
             myData += `<p>${key}: ${value}</p>`;
-           
-          }
-  
+
   });
         myCart = `
           <p><b>Cart Contents</b></p>
